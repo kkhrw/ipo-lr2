e3c228eecb7a3e367b33b4b45367b9736e9872a0
Короленко Ульяна <uljana.korolenko@gmail.com>
2025-10-01 09:42:55 +0300

    Добавлена папка

---
+++ task_1/main.py
@@ -0,0 +1,10 @@
+var_int = 70
+var_float = 8.4
+var_str = "No"
+big_int35 = var_int * 3.5
+var_float-=1
+var_int/var_float
+big_int35/var_float
+var_str = var_str*3+ "Yes"*2
+print(big_int35," ",var_float," ",var_int/var_float
+" ",big_int35/var_float," ",var_str)
