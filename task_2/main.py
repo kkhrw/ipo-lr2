0b4b73a6d878c3a586465a78fd597b8dbac90832
uljana.korolenko@gmail.com <uljana.korolenko@gmail.com>
2025-10-01 22:18:01 +0300

    Задание2

--- task_2/main.py
+++ task_2/main.py
@@ -1,9 +1,7 @@
-import math
-
-n = 4
-k = 9
+n=int(input("Введите кол-во школьников: "))
+k=int(input("Введите кол-во яблок: "))
 apple = k // n
-left = k - (k // n)
+left = k - (n*apple)
 print("Достанется ", apple, " яблок", 
 "Останется ", left, " яблок")
 
