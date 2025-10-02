327c21d9314fbd8b265037529702a2b4822b1e1a
uljana.korolenko@gmail.com <uljana.korolenko@gmail.com>
2025-10-02 18:47:19 +0300

    Задание 4

---
+++ task_4/main.py
@@ -0,0 +1,7 @@
+import math
+#Вариант 2
+x = float(input("Введите значение х: "))
+y = float(input("Введите значение y: "))
+z = float(input("Введите значение z: "))
+w = math.pow(math.fabs(math.cos(x)-math.cos(y)),(1+2*math.pow(math.sin(y),2)))*(1+z+(math.pow(z,2)/2)+(math.pow(z,3)/3)+(math.pow(z,4)/4))
+print("w= ",w)
